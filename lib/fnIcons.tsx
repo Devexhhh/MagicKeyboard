96e0848f9214aa4d2b25b25dc14f8065ec1ea1c1
Devexhhh <kumarsujal.codes@gmail.com>
2026-02-03 01:38:44 +0530

    added icons

---
+++ lib/fnIcons.tsx
@@ -0,0 +1,30 @@
+import type { ReactNode } from "react";
+import {
+    SunDim,
+    Sun,
+    LayoutGrid,
+    Search,
+    Mic,
+    Moon,
+    SkipBack,
+    Play,
+    SkipForward,
+    VolumeX,
+    Volume1,
+    Volume2,
+} from "lucide-react";
+
+export const fnIcons: Record<string, ReactNode> = {
+    F1: <SunDim size={16} />,
+    F2: <Sun size={16} />,
+    F3: <LayoutGrid size={16} />,
+    F4: <Search size={16} />,
+    F5: <Mic size={16} />,
+    F6: <Moon size={16} />,
+    F7: <SkipBack size={16} />,
+    F8: <Play size={16} />,
+    F9: <SkipForward size={16} />,
+    F10: <VolumeX size={16} />,
+    F11: <Volume1 size={16} />,
+    F12: <Volume2 size={16} />,
+};
