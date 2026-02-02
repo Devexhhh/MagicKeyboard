51b43b648eed0ff60e882946eb24dde26e2cc147
Devexhhh <kumarsujal.codes@gmail.com>
2026-02-03 01:18:09 +0530

    init project

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Keyboard } from "./components/keyboard/Keyboard";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen w-screen flex items-center justify-center bg-linear-to-br from-sky-100 to-sky-200">
+      <Keyboard />
+    </main>
+  );
+}
+
