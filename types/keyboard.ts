dbb8945b8c87a311e4ccf38a446c250945b14ebb
Devexhhh <kumarsujal.codes@gmail.com>
2026-02-03 01:20:56 +0530

    added keyboard key types

---
+++ types/keyboard.ts
@@ -0,0 +1,17 @@
+export type KeyWidth = "sm" | "md" | "lg" | "xl" | "xll" | "space" | "cm";
+
+export type KeyAlign =
+    | "center"
+    | "start"
+    | "end"
+    | "baseline-start"
+    | "baseline-end";
+
+export type KeyConfig = {
+    label?: string;
+    top?: string;
+    bottom?: string;
+    width?: KeyWidth;
+    align?: KeyAlign;
+};
+
