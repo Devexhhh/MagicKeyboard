9a9b3b150c849ff7eba6f209defe7a0c94fa4693
Devexhhh <kumarsujal.codes@gmail.com>
2026-02-04 22:59:31 +0530

    added grid background and shadow vignette effect

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,20 @@ import { Keyboard } from "./components/keyboard/Keyboard";
 
 export default function Home() {
   return (
-    <main className="min-h-screen w-screen flex items-center justify-center bg-linear-to-br from-sky-100 to-sky-200">
+    <main className="relative
+    min-h-screen
+    flex items-center justify-center
+    overflow-hidden
+
+    bg-zinc-100
+    bg-[linear-gradient(to_right,rgba(0,0,0,0.08)_1px,transparent_1px),linear-gradient(to_bottom,rgba(0,0,0,0.08)_1px,transparent_1px)]
+    bg-size-[32px_32px]
+
+    before:content-['']
+    before:absolute
+    before:inset-0
+    before:pointer-events-none
+    before:bg-[radial-gradient(circle_at_center,rgba(0,0,0,0.10)_55%,rgba(0,0,0,0.5)_100%)]">
       <Keyboard />
     </main>
   );
