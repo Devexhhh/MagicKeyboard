4257b880e5166596a23e940c627b2f83c26d514c
Devexhhh <kumarsujal.codes@gmail.com>
2026-02-04 22:58:48 +0530

    arrow keys fixed

--- app/components/keyboard/Keyboard.tsx
+++ app/components/keyboard/Keyboard.tsx
@@ -1,18 +1,14 @@
 import { keyboardLayout } from "./keyboard.layout";
 import { KeyboardRow } from "./KeyboardRow";
-import { ArrowCluster } from "./ArrowCluster";
 
 export function Keyboard() {
     return (
-        <div className="bg-black p-2 rounded-xl shadow-2xl">
-            <div className="grid grid-cols-[auto_auto] gap-x-1.5">
-                <div className="flex flex-col gap-1.5 mb-[2px]">
+        <div className="bg-black p-2 rounded-xl shadow-xl">
+            <div className="grid grid-cols-[auto_auto]">
+                <div className="flex flex-col gap-1.5">
                     {keyboardLayout.map((row, idx) => (
                         <KeyboardRow key={idx} row={row} />
                     ))}
-                </div>
-                <div className="flex flex-col gap-1.5 justify-end -ml-53">
-                    <ArrowCluster />
                 </div>
             </div>
         </div>
