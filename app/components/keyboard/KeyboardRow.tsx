5e517aecb0ae07df3d7252a9534e14a7d0cc1723
Devexhhh <kumarsujal.codes@gmail.com>
2026-02-04 22:58:33 +0530

    Arrow Cluster alignment fix

--- app/components/keyboard/KeyboardRow.tsx
+++ app/components/keyboard/KeyboardRow.tsx
@@ -1,26 +1,20 @@
 import { Key } from "./Key";
+import { ArrowCluster } from "./ArrowCluster";
 import { KeyConfig } from "@/types/keyboard";
 
 export function KeyboardRow({ row }: { row: KeyConfig[] }) {
-    const arrowKeys = row.filter((k) => k.arrow);
-    const normalKeys = row.filter((k) => !k.arrow);
-
     return (
         <div className="flex gap-1.5">
-            {normalKeys.map((key, idx) => (
-                <Key key={idx} {...key} />
-            ))}
-            {arrowKeys.length > 0 && (
-                <div className="grid grid-cols-3 gap-1.5">
-                    <div />
-                    <Key arrow="up" />
-                    <div />
-
-                    <Key arrow="left" />
-                    <Key arrow="down" />
-                    <Key arrow="right" />
-                </div>
-            )}
+            {row.map((item, idx) => {
+                if ("type" in item && item.type === "arrowCluster") {
+                    return (
+                        <div key={idx} className="flex justify-center">
+                            <ArrowCluster />
+                        </div>
+                    );
+                }
+                return <Key key={idx} {...item} />;
+            })}
         </div>
     );
 }
