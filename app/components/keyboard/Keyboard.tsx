ad7bfdbd50feb6e9480a1982d87c8576b59ef610
Devexhhh <kumarsujal.codes@gmail.com>
2026-02-04 21:09:10 +0530

    fixed Keyboard size

--- app/components/keyboard/Keyboard.tsx
+++ app/components/keyboard/Keyboard.tsx
@@ -1,13 +1,19 @@
 import { keyboardLayout } from "./keyboard.layout";
 import { KeyboardRow } from "./KeyboardRow";
+import { ArrowCluster } from "./ArrowCluster";
 
 export function Keyboard() {
     return (
-        <div className="bg-black p-2 rounded-2xl shadow-2xl">
-            <div className="flex flex-col gap-1.5">
-                {keyboardLayout.map((row, idx) => (
-                    <KeyboardRow key={idx} row={row} />
-                ))}
+        <div className="bg-black p-2 rounded-xl shadow-2xl">
+            <div className="grid grid-cols-[auto_auto] gap-x-1.5">
+                <div className="flex flex-col gap-1.5 mb-[2px]">
+                    {keyboardLayout.map((row, idx) => (
+                        <KeyboardRow key={idx} row={row} />
+                    ))}
+                </div>
+                <div className="flex flex-col gap-1.5 justify-end -ml-53">
+                    <ArrowCluster />
+                </div>
             </div>
         </div>
     );
