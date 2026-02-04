3fc6019ba20e9b0f41d288549443b0657d367035
Devexhhh <kumarsujal.codes@gmail.com>
2026-02-04 21:09:40 +0530

    fixed Keyboard Rows

--- app/components/keyboard/KeyboardRow.tsx
+++ app/components/keyboard/KeyboardRow.tsx
@@ -2,11 +2,25 @@ import { Key } from "./Key";
 import { KeyConfig } from "@/types/keyboard";
 
 export function KeyboardRow({ row }: { row: KeyConfig[] }) {
+    const arrowKeys = row.filter((k) => k.arrow);
+    const normalKeys = row.filter((k) => !k.arrow);
+
     return (
         <div className="flex gap-1.5">
-            {row.map((key, idx) => (
+            {normalKeys.map((key, idx) => (
                 <Key key={idx} {...key} />
             ))}
+            {arrowKeys.length > 0 && (
+                <div className="grid grid-cols-3 gap-1.5">
+                    <div />
+                    <Key arrow="up" />
+                    <div />
+
+                    <Key arrow="left" />
+                    <Key arrow="down" />
+                    <Key arrow="right" />
+                </div>
+            )}
         </div>
     );
 }
