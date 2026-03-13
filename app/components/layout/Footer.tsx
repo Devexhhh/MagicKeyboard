c318e88f49dcef6b83b74ba8c0ca0e0eb814a8e7
Devexhhh <kumarsujal.codes@gmail.com>
2026-03-13 20:08:48 +0530

    fixed responsiveness

--- app/components/layout/Footer.tsx
+++ app/components/layout/Footer.tsx
@@ -4,7 +4,7 @@ export function Footer() {
             className="
       relative
       w-full
-      h-25
+      h-12
       flex items-center justify-center
       text-sm
       font-(--font-funnel)
@@ -13,12 +13,12 @@ export function Footer() {
       bg-zinc-900 text-white
 
       bg-[linear-gradient(to_right,rgba(255,255,255,0.08)_1px,transparent_1px),linear-gradient(to_bottom,rgba(255,255,255,0.08)_1px,transparent_1px)]
-      bg-size-[50px_50px]
+      bg-size-[20px_20px]
 
       border-t border-white/10
       "
         >
-            <p className="text-xl tracking-tighter text-zinc-300">
+            <p className="text-sm tracking-tighter text-zinc-300">
                 © {new Date().getFullYear()} Devex — Magic Keyboard UI
             </p>
         </footer >
